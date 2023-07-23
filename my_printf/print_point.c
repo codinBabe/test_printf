896d64307a5b476ee15becfc66688919d413920f
codinBabe <teboola2@hmail.com>
2023-07-22 20:22:39 -0700

    hopefully fix task 6

--- my_printf/print_point.c
+++ my_printf/print_point.c
@@ -6,19 +6,26 @@
  */
 int print_pointer(va_list p)
 {
-	void *ptr = va_arg(p, void *);
-	size_t address;
-	int i, hex_digit;
+	uintptr_t address = (uintptr_t)va_arg(p, void *);
+	uintptr_t temp;
+	int i, hex_digit, num_digits;
 
-	address = (size_t)ptr;
 	_putchar('0');
 	_putchar('x');
-	for (i = (sizeof(size_t) * 2) - 1; i >= 0; i--)
+
+	num_digits = 0;
+	temp = address;
+	do {
+		temp >>= 4;
+		num_digits++;
+	} while (temp != 0);
+
+	for (i = num_digits - 1; i >= 0; i--)
 	{
 		hex_digit = (address >> (i * 4)) & 0xF;
 		_putchar(hex_digit < 10 ? hex_digit + '0' :
 				hex_digit - 10 + 'a');
 	}
 
-	return (18);
+	return (num_digits + 2);
 }
